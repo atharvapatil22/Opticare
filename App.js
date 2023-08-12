ded07837abc8bf0b934689dadf2624f8dd0c5655
atharvapatil22 <atharva.22.patil@gmail.com>
2023-08-12 14:04:12 +0530

    project setup

--- App.js
+++ App.js
@@ -1,7 +1,19 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import { useEffect } from "react";
+import * as ScreenOrientation from "expo-screen-orientation";
 
 export default function App() {
+  useEffect(() => {
+    lockScreenOrientation();
+  }, []);
+
+  async function lockScreenOrientation() {
+    await ScreenOrientation.lockAsync(
+      ScreenOrientation.OrientationLock.LANDSCAPE_RIGHT
+    );
+  }
+
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
@@ -13,8 +25,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
