796448674597e569537be5e6e224c60986cfce58
atharvapatil22 <atharva.22.patil@gmail.com>
2023-08-16 10:16:37 +0530

    topbar ui completed

---
+++ src/components/TempButton.jsx
@@ -0,0 +1,26 @@
+import { Text, TouchableOpacity } from "react-native";
+import React from "react";
+import { aqua1, grey1 } from "../constants";
+
+const TempButton = ({ text, variant }) => {
+  return (
+    <TouchableOpacity
+      style={{
+        paddingHorizontal: 20,
+        paddingVertical: 10,
+        borderRadius: 24,
+        borderWidth: variant === "white" ? 1 : 0,
+        borderColor: variant === "white" ? grey1 : "transparent",
+        backgroundColor: variant === "aqua" ? aqua1 : "white",
+      }}
+    >
+      <Text
+        style={{ fontSize: 18, color: variant === "aqua" ? "white" : "black" }}
+      >
+        {text}
+      </Text>
+    </TouchableOpacity>
+  );
+};
+
+export default TempButton;
--- src/screens/Spectacles/Spectacles.jsx
+++ src/screens/Spectacles/Spectacles.jsx
@@ -1,32 +1,25 @@
-import {
-  View,
-  Text,
-  StyleSheet,
-  TextInput,
-  TouchableOpacity,
-} from "react-native";
+import { View, Text, StyleSheet, TextInput } from "react-native";
 import React, { useState } from "react";
+import { grey1 } from "../../constants";
+import TempButton from "../../components/TempButton";
 
 const Spectacles = () => {
   const [searchValue, setSearchValue] = useState("");
+
   return (
-    <View>
+    <View style={{ backgroundColor: "#F5F8FA" }}>
       {/* TOPBAR */}
       <View style={styles.topbar}>
         <TextInput
           style={styles.searchbar}
           onChangeText={setSearchValue}
           value={searchValue}
+          placeholder="Type here to search ..."
+          placeholderTextColor={grey1}
         />
-        <TouchableOpacity>
-          <Text>Search</Text>
-        </TouchableOpacity>
-        <TouchableOpacity>
-          <Text>Filters</Text>
-        </TouchableOpacity>
-        <TouchableOpacity>
-          <Text>+ Add New</Text>
-        </TouchableOpacity>
+        <TempButton text="SEARCH" variant="aqua" />
+        <TempButton text="Filters" variant="white" />
+        <TempButton text="+ ADD NEW" variant="aqua" />
       </View>
       <Text>Spectacles</Text>
     </View>
@@ -36,6 +29,20 @@ const Spectacles = () => {
 export default Spectacles;
 
 const styles = StyleSheet.create({
-  topbar: { backgroundColor: "red" },
-  searchbar: { borderWidth: 1 },
+  topbar: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    paddingVertical: 20,
+    borderBottomWidth: 1.5,
+    borderBottomColor: grey1,
+    marginHorizontal: "2%",
+  },
+  searchbar: {
+    borderWidth: 1,
+    width: "60%",
+    borderColor: grey1,
+    borderRadius: 24,
+    fontSize: 18,
+    paddingHorizontal: 20,
+  },
 });
