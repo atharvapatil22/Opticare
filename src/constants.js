5003257c4a93d5f95f7fc6aec67fe3d529a398d1
atharvapatil22 <atharva.22.patil@gmail.com>
2023-08-19 03:02:29 +0530

    Added grid for product list

--- src/constants.js
+++ src/constants.js
@@ -2,3 +2,4 @@ export const aqua1 = "#26A69A";
 export const aqua2 = "#4DB6AC";
 export const grey1 = "#D5D5D5";
 export const grey2 = "#F5F8FA";
+export const grey3 = "rgba(38, 46, 46, 0.5)";
--- src/screens/Spectacles/Spectacles.jsx
+++ src/screens/Spectacles/Spectacles.jsx
@@ -1,6 +1,13 @@
-import { View, Text, StyleSheet, TextInput } from "react-native";
+import {
+  View,
+  Text,
+  StyleSheet,
+  TextInput,
+  Image,
+  ScrollView,
+} from "react-native";
 import React, { useEffect, useState } from "react";
-import { grey1 } from "../../constants";
+import { grey1, grey3 } from "../../constants";
 import TempButton from "../../components/TempButton";
 import { supabase } from "../../supabase/client";
 
@@ -13,13 +20,39 @@ const Spectacles = () => {
   }, []);
 
   const fetchSpecs = async () => {
-    const { data, error } = await supabase.from("spectacles").select("name");
+    const { data, error } = await supabase.from("spectacles").select("*");
     if (error) {
       // api_error
       console.log("api_error");
     } else {
+      // api_success
       setSpecs(data);
     }
+  };
+
+  const SpecsCard = ({ data }) => {
+    return (
+      <View style={styles.card}>
+        <Image
+          source={{
+            uri: data.preview_image,
+          }}
+          style={{
+            aspectRatio: "16/9",
+            objectFit: "fill",
+            width: "100%",
+            borderRadius: 30,
+          }}
+        />
+        <Text style={{ fontSize: 18, color: "black" }}>{data.name}</Text>
+        <View style={{ flexDirection: "row" }}>
+          <Text style={{ fontSize: 16, color: "black" }}>₹{data.price}</Text>
+          <Text style={{ fontSize: 16, color: grey3, marginLeft: 8 }}>
+            ({data.lens_options} Lens options)
+          </Text>
+        </View>
+      </View>
+    );
   };
 
   return (
@@ -37,7 +70,13 @@ const Spectacles = () => {
         <TempButton text="Filters" variant="white" />
         <TempButton text="+ ADD NEW" variant="aqua" />
       </View>
-      <Text>Spectacles</Text>
+      <ScrollView style={{ width: "100%", height: "100%" }}>
+        <View style={styles.grid_container}>
+          {specs.map((item) => (
+            <SpecsCard data={item} key={item.id} />
+          ))}
+        </View>
+      </ScrollView>
     </View>
   );
 };
@@ -60,5 +99,23 @@ const styles = StyleSheet.create({
     borderRadius: 24,
     fontSize: 18,
     paddingHorizontal: 20,
+  },
+  grid_container: {
+    flexWrap: "wrap",
+    flexDirection: "row",
+    paddingTop: 20,
+    paddingBottom: 50,
+    paddingHorizontal: 20,
+    // height: "100%",
+  },
+  card: {
+    flexBasis: "23%",
+    // height: 240,
+    margin: "1%",
+    borderRadius: 30,
+    padding: "1%",
+    paddingBottom: "2%",
+    backgroundColor: "white",
+    alignItems: "center",
   },
 });
