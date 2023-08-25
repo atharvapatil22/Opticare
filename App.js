11e2a9a972254bca16e12c3beba8e714fe16dd57
atharvapatil22 <atharva.22.patil@gmail.com>
2023-08-26 04:04:03 +0530

    fixed some npm errors

--- App.js
+++ App.js
@@ -3,8 +3,6 @@ import { useEffect } from "react";
 import * as ScreenOrientation from "expo-screen-orientation";
 import { NavigationContainer } from "@react-navigation/native";
 import MainNavigator from "./src/navigators/MainNavigator";
-import * as eva from "@eva-design/eva";
-import { ApplicationProvider } from "@ui-kitten/components";
 
 export default function App() {
   useEffect(() => {
@@ -18,10 +16,8 @@ export default function App() {
   }
 
   return (
-    <ApplicationProvider {...eva} theme={eva.light}>
-      <NavigationContainer>
-        <MainNavigator />
-      </NavigationContainer>
-    </ApplicationProvider>
+    <NavigationContainer>
+      <MainNavigator />
+    </NavigationContainer>
   );
 }
