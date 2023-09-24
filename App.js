c3e7d8fea774ede14e5e0d9273839b0aa23b1762
atharvapatil22 <atharva.22.patil@gmail.com>
2023-09-25 02:02:00 +0530

    updated icon and splash screen

--- App.js
+++ App.js
@@ -1,32 +1,51 @@
 import "react-native-gesture-handler";
-import { useEffect, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 import * as ScreenOrientation from "expo-screen-orientation";
 import { NavigationContainer } from "@react-navigation/native";
 import MainNavigator from "./src/navigators/MainNavigator";
 import { Provider } from "react-redux";
 import store from "./src/redux/store";
 import * as Font from "expo-font";
-import Apploading from "expo-app-loading";
 import { PaperProvider } from "react-native-paper";
+import * as SplashScreen from "expo-splash-screen";
+import { View } from "react-native";
+import { Text } from "react-native";
 
 export default function App() {
-  const [fontsLoaded, setFontsLoaded] = useState(false);
-  useEffect(() => {
-    lockScreenOrientation();
-  }, []);
+  // Keep the splash screen visible while we fetch resources
+  SplashScreen.preventAutoHideAsync();
 
-  const getFonts = () =>
-    Font.loadAsync({
-      "Inter-Thin": require("./assets/fonts/Inter-Thin.ttf"),
-      "Inter-ExtraLight": require("./assets/fonts/Inter-ExtraLight.ttf"),
-      "Inter-Light": require("./assets/fonts/Inter-Light.ttf"),
-      "Inter-Regular": require("./assets/fonts/Inter-Regular.ttf"),
-      "Inter-Medium": require("./assets/fonts/Inter-Medium.ttf"),
-      "Inter-SemiBold": require("./assets/fonts/Inter-SemiBold.ttf"),
-      "Inter-Bold": require("./assets/fonts/Inter-Bold.ttf"),
-      "Inter-ExtraBold": require("./assets/fonts/Inter-ExtraBold.ttf"),
-      "Inter-Black": require("./assets/fonts/Inter-Black.ttf"),
-    });
+  const [appIsReady, setAppIsReady] = useState(false);
+
+  useEffect(() => {
+    async function prepare() {
+      try {
+        // Pre-load fonts, make any API calls you need to do here
+        await lockScreenOrientation();
+        await Font.loadAsync({
+          "Inter-Thin": require("./assets/fonts/Inter-Thin.ttf"),
+          "Inter-ExtraLight": require("./assets/fonts/Inter-ExtraLight.ttf"),
+          "Inter-Light": require("./assets/fonts/Inter-Light.ttf"),
+          "Inter-Regular": require("./assets/fonts/Inter-Regular.ttf"),
+          "Inter-Medium": require("./assets/fonts/Inter-Medium.ttf"),
+          "Inter-SemiBold": require("./assets/fonts/Inter-SemiBold.ttf"),
+          "Inter-Bold": require("./assets/fonts/Inter-Bold.ttf"),
+          "Inter-ExtraBold": require("./assets/fonts/Inter-ExtraBold.ttf"),
+          "Inter-Black": require("./assets/fonts/Inter-Black.ttf"),
+        });
+
+        // Artificially delay for two seconds to simulate a slow loading
+        // experience. Please remove this if you copy and paste the code!
+      } catch (e) {
+        console.warn(e);
+      } finally {
+        // Tell the application to render
+        setAppIsReady(true);
+      }
+    }
+
+    prepare();
+  }, []);
 
   async function lockScreenOrientation() {
     await ScreenOrientation.lockAsync(
@@ -34,8 +53,27 @@ export default function App() {
     );
   }
 
-  if (fontsLoaded) {
+  const onLayoutRootView = useCallback(async () => {
+    if (appIsReady) {
+      // This tells the splash screen to hide immediately! If we call this after
+      // `setAppIsReady`, then we may see a blank screen while the app is
+      // loading its initial state and rendering its first pixels. So instead,
+      // we hide the splash screen once we know the root view has already
+      // performed layout.
+      await SplashScreen.hideAsync();
+    }
+  }, [appIsReady]);
+
+  if (!appIsReady) {
     return (
+      <View>
+        <Text style={{ fontSize: 30, color: "black" }}>Splash</Text>
+      </View>
+    );
+  }
+
+  return (
+    <View onLayout={onLayoutRootView} style={{ width: "100%", height: "100%" }}>
       <Provider store={store}>
         <PaperProvider>
           <NavigationContainer>
@@ -43,16 +81,6 @@ export default function App() {
           </NavigationContainer>
         </PaperProvider>
       </Provider>
-    );
-  } else {
-    return (
-      <Apploading
-        startAsync={getFonts}
-        onFinish={() => {
-          setFontsLoaded(true);
-        }}
-        onError={console.warn}
-      />
-    );
-  }
+    </View>
+  );
 }
